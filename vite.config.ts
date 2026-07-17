ae2c0b9191ce1f5204e0f3e35df7f7e42c5932a9
Igor Bonato <igorbonato99@hotmail.com>
2026-07-17 11:55:10 -0300

    Change default site title from "Figma Make App" to "English Grammar in Use"

--- vite.config.ts
+++ vite.config.ts
@@ -94,7 +94,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? "Figma Make App"
+  const title = config.title ?? "English Grammar in Use"
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
